531a5952bc0beb7f881c8ff778e6871dae6b16e5
StanimirSofronovMG <stanimir.sofronov@gmail.com>
2023-04-18 22:43:00 +0300

    .

--- StanimirSofronov08.04.2023/StanimirSofronov08.04.2023/ReportsForm.cs
+++ StanimirSofronov08.04.2023/StanimirSofronov08.04.2023/ReportsForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,8 @@ namespace StanimirSofronov08._04._2023
 {
     public partial class ReportsForm : Form
     {
+        private  RestaurantContext _context;
+
         public ReportsForm()
         {
             InitializeComponent();
@@ -20,6 +24,25 @@ namespace StanimirSofronov08._04._2023
         private void Report1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            _context = new RestaurantContext();
+
+            _context.Database.EnsureCreated();
+
+
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            _context?.Dispose();
+            _context = null;
         }
     }
 }
